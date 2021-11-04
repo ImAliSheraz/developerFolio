0bb6003d8b48e65ab3d182a6b9c6a360607b7e11
ImAliSheraz <alisheraz7777@gmail.com>
2021-11-04 06:40:33 +0000

    Deploying to gh-pages from  @ 639b944664f11608facf24d4b14e28fc286086bf 🚀

---
+++ precache-manifest.7875353bd1b87d83fb11cca8fb677d5f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79b89ca87270b837d51f3d4eb3c3fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79b282949db1ee3d3345",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "aaedd68e31ad50a00ea9",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "bf8e8a9b3b65aa3afa77",
+    "url": "/static/css/main.4ca95abf.chunk.css"
+  },
+  {
+    "revision": "be05e8099f413062492a",
+    "url": "/static/js/2.cbcd3007.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.cbcd3007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b282949db1ee3d3345",
+    "url": "/static/js/3.f4f460ff.chunk.js"
+  },
+  {
+    "revision": "aaedd68e31ad50a00ea9",
+    "url": "/static/js/4.94515272.chunk.js"
+  },
+  {
+    "revision": "bf8e8a9b3b65aa3afa77",
+    "url": "/static/js/main.4b7d5f43.chunk.js"
+  },
+  {
+    "revision": "76ef62f914efd2a55f1e",
+    "url": "/static/js/runtime-main.d5ec6992.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
